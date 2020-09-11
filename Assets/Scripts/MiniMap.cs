5747bcf20bf9265fd25d9e57ec629418cba85f0a
FU ELM <rikkedomino@gmail.com>
2020-09-11 14:12:23 +0200

    opdatering

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=28JTTXqMvOU&t=7s
+public class MiniMap : MonoBehaviour {
+     public Transform player;
+
+    void LateUpdate (){
+
+        //minimap follow the player
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        //minimap are rotation
+        //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+
+    }
+
+
+
+
+
+
+}
