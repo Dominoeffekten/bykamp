ec9a4a2b46f3bf146f5a16e0b8e829a9b51ecc03
FU ELM <rikkedomino@gmail.com>
2020-10-05 14:20:44 +0200

    farver og døre

--- Assets/Scripts/DoorGrabbable.cs
+++ Assets/Scripts/DoorGrabbable.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//open door
 // https://www.youtube.com/watch?v=3cJ_uq1m-dg&vl=fr
 
+
+//lyd
+// https://answers.unity.com/questions/459651/play-sound-when-opening-door.html
+
 //grabble the door
-public class DoorGrabbable : OVRGrabbable
-{
+public class DoorGrabbable : OVRGrabbable {
     public Transform handler;
+
 	
 	//tager firkanten i døren, og sætter den på sin rette plads igen, så den ikke svæver i luften
     public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity){ //linearVelocity is X and Y directions angularVelocity is Angular velocity in degrees per second.
 
-	base.GrabEnd(Vector3.zero, Vector3.zero);
-	transform.position = handler.transform.position;
-	transform.rotation = handler.transform.rotation;
+		base.GrabEnd(Vector3.zero, Vector3.zero);
+		transform.position = handler.transform.position;
+		transform.rotation = handler.transform.rotation;
 
-	//undgår at døren går tilbage igen
-	Rigidbody rbhandler = handler.GetComponent<Rigidbody>(); //physics simulation
-	rbhandler.velocity = Vector3.zero; //The velocity vector of the rigidbody. It represents the rate of change of Rigidbody position.
-	rbhandler.angularVelocity = Vector3.zero; // The angular velocity vector of the rigidbody measured in radians per second.
-    }
+		//undgår at døren går tilbage igen
+		Rigidbody rbhandler = handler.GetComponent<Rigidbody>(); //physics simulation
+		rbhandler.velocity = Vector3.zero; //The velocity vector of the rigidbody. It represents the rate of change of Rigidbody position.
+		rbhandler.angularVelocity = Vector3.zero; // The angular velocity vector of the rigidbody measured in radians per second.
+	}
+
 
 	private void Update(){
 		if (Vector3.Distance(handler.position, transform.position) > 0.4f){
--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//åben døren
 // https://www.youtube.com/watch?v=3cJ_uq1m-dg&vl=fr
-
 
 //open door
 public class OpenDoor : MonoBehaviour
 {
-
 	public Transform target;
 	Rigidbody rb;
-
+    AudioSource sound;
+    bool playSound;
+    
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
+        sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-	rb.MovePosition(target.transform.position);
-        
+    void FixedUpdate() {
+	    rb.MovePosition(target.transform.position); 
+        MakeSound();
+    } 
+    void MakeSound(){
+        sound.Play(); 
     }
 }
